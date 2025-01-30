92d8e9371a1dc6bdbfb3fc03bc29d45227ada6cc
nodbew <165175929+nodbew@users.noreply.github.com>
2025-01-30 17:47:19 +0900

    Migrate to vitest

---
+++ vitest-setup.ts
@@ -0,0 +1,39 @@
+import "@testing-library/jest-dom/vitest";
+import { cleanup } from "@testing-library/react";
+import { afterEach } from "vitest";
+import { vi } from "vitest";
+
+global.ResizeObserver = vi.fn().mockImplementation(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
+window.HTMLElement.prototype.scrollIntoView = vi.fn();
+
+function createMockPointerEvent(
+  type: string,
+  props: PointerEventInit = {}
+): PointerEvent {
+  const event = new Event(type, props) as PointerEvent;
+  Object.assign(event, {
+    button: props.button ?? 0,
+    ctrlKey: props.ctrlKey ?? false,
+    pointerType: props.pointerType ?? "mouse",
+  });
+  return event;
+}
+
+// Assign the mock function to the global window object
+window.PointerEvent = createMockPointerEvent as any;
+
+// Mock HTMLElement methods
+Object.assign(window.HTMLElement.prototype, {
+  scrollIntoView: vi.fn(),
+  releasePointerCapture: vi.fn(),
+  hasPointerCapture: vi.fn(),
+});
+
+// runs a cleanup after each test case (e.g. clearing jsdom)
+afterEach(() => {
+  cleanup();
+});
